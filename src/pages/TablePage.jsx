7e63ac7db1ad58c4029045e74a6702106034498d
artemchernii <artemcherniy7@gmail.com>
2023-04-21 11:05:16 +0100

    refactor: refactor table and sortable table

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,37 +1,33 @@
-import SortableTable from '../components/SortableTable';
+import SortableTable from "../components/SortableTable";
 
 const fruits = [
-    { name: 'Orange', color: 'bg-orange-500', score: 5 },
-    { name: 'Apple', color: 'bg-red-500', score: 3 },
-    { name: 'Banana', color: 'bg-yellow-500', score: 1 },
-    { name: 'Lime', color: 'bg-green-500', score: 4 },
+  { name: "Orange", color: "bg-orange-500", score: 5 },
+  { name: "Apple", color: "bg-red-500", score: 3 },
+  { name: "Banana", color: "bg-yellow-500", score: 1 },
+  { name: "Lime", color: "bg-green-500", score: 4 },
 ];
 
 const config = [
-    {
-        label: 'Name',
-        render: (fruit) => fruit.name,
-        sortValue: (fruit) => fruit.name,
-    },
-    {
-        label: 'Color',
-        render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
-    },
-    {
-        sortValue: (fruit) => fruit.score,
-        label: 'Score',
-        render: (fruit) => fruit.score,
-    },
+  {
+    label: "Name",
+    render: (fruit) => fruit.name,
+    sortValue: (fruit) => fruit.name,
+  },
+  {
+    label: "Color",
+    render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+  },
+  {
+    label: "Score",
+    render: (fruit) => fruit.score,
+    sortValue: (fruit) => fruit.score,
+  },
 ];
 
 const keyFn = (fruit) => {
-    return fruit.name;
+  return fruit.name;
 };
 
 export default function TablePage() {
-    return (
-        <div>
-            <SortableTable data={fruits} config={config} keyFn={keyFn} />
-        </div>
-    );
+  return <SortableTable data={fruits} config={config} keyFn={keyFn} />;
 }
